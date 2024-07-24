44d0b5005f24c69428f28f68d589c593dcd99554
GauthamPrakash14 <36693382+GauthamPrakash14@users.noreply.github.com>
2024-07-24 18:27:52 +0100

    Add links to socials

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 import { TypeAnimation } from "react-type-animation";
-import { FaTwitter, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
+import { FaXTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa6'
+import image from "../assets/bg.jpg"
 
 const Image = () => {
 
     return(
         <div id="main">
-            <img className="w-full h-screen object-cover object-left" src="https://images.unsplash.com/photo-1675495666589-94cdafbcfcc8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className="w-full h-screen object-cover object-left" src={image} alt="" />
             <div className="w-full h-screen absolute top-0 left-0 bg-white opacity-50">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
                 <h1 className="sm:text-5xl text-4xl font-bold text-gray-800"> I'm Gautham Prakash</h1>
@@ -29,9 +30,15 @@ const Image = () => {
                     />
                 </h2>
                 <div className="flex justify-between pt-6 max-w-[200px] w-full">
-                    <FaTwitter className="cursor-pointer" size={20} />
-                    <FaInstagram className="cursor-pointer" size={20} />
-                    <FaLinkedinIn className="cursor-pointer" size={20} />
+                    <a href="https://x.com/Gautham847" target="_blank" rel="noopener">
+                        <FaXTwitter className="cursor-pointer" size={20} />
+                    </a>
+                    <a href="https://www.instagram.com/_gautham________/?next=%2F" target="_blank" rel="noopener">
+                        <FaInstagram className="cursor-pointer" size={20} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/gauthamprakash/" target="_blank" rel="noopener">
+                        <FaLinkedin className="cursor-pointer" size={20}/>
+                    </a>
                 </div>
             </div>
             </div>
