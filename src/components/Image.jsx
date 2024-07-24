932a82b05924a449901c3c86ed0b4b0eedb8646e
GauthamPrakash14 <36693382+GauthamPrakash14@users.noreply.github.com>
2024-07-24 17:58:16 +0100

    Fix for bg image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -6,7 +6,7 @@ const Image = () => {
 
     return(
         <div id="main">
-            <img className="w-full h-screen object-cover object-left scale-x-[-1]" src="https://source.unsplash.com/Im7lZjxeLhg" alt="" />
+            <img className="w-full h-screen object-cover object-left" src="https://images.unsplash.com/photo-1675495666589-94cdafbcfcc8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <div className="w-full h-screen absolute top-0 left-0 bg-white opacity-50">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
                 <h1 className="sm:text-5xl text-4xl font-bold text-gray-800"> I'm Gautham Prakash</h1>
